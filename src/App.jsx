e7fb0b1bf173aa8df3c6b9c367e240bbbb6010a4
kiruthiga-07 <kiruthigaraja07@gmail.com>
2026-09-24 10:13:53 +0530

    Test CI failure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <h1>Hello CI/CD</h1>
-      <p>This application is built automatically.</p>
+      <p>This application is built automatically.
     </div>
   );
 }

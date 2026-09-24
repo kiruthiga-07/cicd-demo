28a7518bbc3acbb404334d1a6f5e2b3f9ecb41d1
kiruthiga-07 <kiruthigaraja07@gmail.com>
2026-09-24 10:17:13 +0530

    Fix build error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <h1>Hello CI/CD</h1>
-      <p>This application is built automatically.
+      <p>This application is built automatically.</p>
     </div>
   );
 }
